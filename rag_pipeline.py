df5d03555d0d43f5b010bf14d1c9743d8b34bab6
Pawan ganesh <148455845+PG113131@users.noreply.github.com>
2026-05-19 21:25:30 +0530

    Update rag_pipeline.py

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -7,34 +7,26 @@ import os
 
 load_dotenv()
 
-# ====================================
 # EMBEDDING MODEL
-# ====================================
 
 embedding_model = HuggingFaceEmbeddings(
     model_name="BAAI/bge-base-en-v1.5"
 )
 
-# ====================================
 # VECTOR DATABASE
-# ====================================
 
 vectordb = Chroma(
     persist_directory="./chroma_db",
     embedding_function=embedding_model
 )
 
-# ====================================
 # RETRIEVER
-# ====================================
 
 retriever = vectordb.as_retriever(
     search_kwargs={"k": 4}
 )
 
-# ====================================
 # LLAMA 3.1
-# ====================================
 
 llm = ChatGroq(
     model_name="llama-3.1-8b-instant",
